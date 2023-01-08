853508e5ccf90b045c7c7e4217ea94386c49357d
Tim Johnsen <timonus@gmail.com>
2023-01-08 12:26:12 -0800

    Make main app so it can share different res photos.

--- ShareExtensionTester/ViewController.m
+++ ShareExtensionTester/ViewController.m
@@ -9,19 +9,38 @@
 
 @interface ViewController ()
 
+@property (copy) NSString *filename;
+
 @end
+
+static NSString *const kFileExtension = @"heic";
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     
-    UIButton *imageDataButton = [[UIButton alloc] initWithFrame:CGRectMake(20, 100, 300, 64)];
+    self.filename = @"48mp";
+    
+    UISegmentedControl *seg = [[UISegmentedControl alloc] initWithFrame:CGRectMake(20, 100, 300, 64) actions:@[
+        [UIAction actionWithTitle:@"48mp" image:nil identifier:nil handler:^(__kindof UIAction * _Nonnull action) {
+        self.filename = @"48mp";
+    }],
+        [UIAction actionWithTitle:@"12mp" image:nil identifier:nil handler:^(__kindof UIAction * _Nonnull action) {
+        self.filename = @"12mp";
+    }],
+        [UIAction actionWithTitle:@"7mp" image:nil identifier:nil handler:^(__kindof UIAction * _Nonnull action) {
+        self.filename = @"7mp";
+    }],
+    ]];
+    seg.selectedSegmentIndex = 0;
+    [self.view addSubview:seg];
+    
+    UIButton *imageDataButton = [[UIButton alloc] initWithFrame:CGRectMake(20, CGRectGetMaxY(seg.frame) + 40, 300, 64)];
     [imageDataButton setTitle:@"Share image NSData" forState:UIControlStateNormal];
     [imageDataButton addAction:[UIAction actionWithHandler:^(__kindof UIAction * _Nonnull action) {
         // Share image as data
-        UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems:@[[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"very-large-photo" ofType:@"jpg"]]] applicationActivities:nil];
+        UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems:@[[NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:self.filename ofType:kFileExtension] options:NSDataReadingMappedIfSafe error:nil]] applicationActivities:nil];
         [self presentViewController:activityViewController animated:YES completion:nil];
     }] forControlEvents:UIControlEventTouchUpInside];
     
@@ -29,7 +48,7 @@
     [imageButton setTitle:@"Share UIImage" forState:UIControlStateNormal];
     [imageButton addAction:[UIAction actionWithHandler:^(__kindof UIAction * _Nonnull action) {
         // Share UIImage
-        UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems:@[[UIImage imageNamed:@"very-large-photo.jpg"]] applicationActivities:nil];
+        UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems:@[[UIImage imageNamed:[NSString stringWithFormat:@"%@.%@", self.filename, kFileExtension]]] applicationActivities:nil];
         [self presentViewController:activityViewController animated:YES completion:nil];
     }] forControlEvents:UIControlEventTouchUpInside];
     
